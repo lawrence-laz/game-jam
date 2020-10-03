daaeb5553330b737ab0447573b876011c6e28442
Laurynas Lazauskas <laurynas@lazauskas.info>
2020-10-03 12:10:56 +0300

    Modigy Gaming Effect

--- LudumDare/LD47/Ludum Dare 47/Assets/Cards/PlayGames/GamingVisualEffects.cs
+++ LudumDare/LD47/Ludum Dare 47/Assets/Cards/PlayGames/GamingVisualEffects.cs
@@ -1,14 +1,25 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class GamingVisualEffects : MonoBehaviour, IVisualEffect
 {
     private void Start()
     {
-        var face = transform.parent.GetComponentInChildren<Face>();
-        if (face != null)
-        {
-            face.SetFace(face.VeryExcided);
-        }
+
+        DOTween.Sequence()
+            .Append(transform.DOShakePosition(3f, 0.01f, 10, 100, fadeOut: false))
+            .Append(transform.DOLocalMove(new Vector3(0.047f, 0.078f, -0.11f), 0.3f))
+            .Join(transform.DOLocalRotate(new Vector3(0, 0, 28.635f), 0.3f))
+            .AppendCallback(() =>
+            {
+                var face = transform.parent.GetComponentInChildren<Face>();
+                if (face != null)
+                {
+                    face.SetFace(face.VeryExcided);
+                }
+            })
+            .Append(transform.DOShakePosition(10f, 0.1f, 20, 100, fadeOut: false))
+            .Play();
     }
 
     private void OnDestroy()
