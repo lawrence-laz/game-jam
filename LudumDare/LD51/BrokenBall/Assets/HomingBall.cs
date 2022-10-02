45f0062f8b8ae5fac3cae66118ca5ff8432791c6
lawrence-laz <laurynas@lazauskas.info>
2022-10-02 13:29:31 +0300

    Add homing ball

---
+++ LudumDare/LD51/BrokenBall/Assets/HomingBall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingBall : MonoBehaviour
+{
+    public Rigidbody2D Body;
+    public Transform Paddle;
+
+    private void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+        Paddle = FindObjectOfType<Paddle>().transform;
+    }
+
+    void LateUpdate()
+    {
+        if (Body.velocity.y < 0)
+        {
+            // Going down, start homing.
+            Body.velocity = Vector2.MoveTowards(Body.velocity, transform.DirectionTo(Paddle) * Body.velocity.magnitude, Time.deltaTime * 20);
+        }
+    }
+}
