da8dfbca25f3b3a802d58b49d80c56a95ca5b700
lawrence-laz <laurynas@lazauskas.info>
2022-10-02 11:57:40 +0300

    Fix extra ball spawn position

--- LudumDare/LD51/BrokenBall/Assets/ExtraBallUpgrade.cs
+++ LudumDare/LD51/BrokenBall/Assets/ExtraBallUpgrade.cs
@@ -6,7 +6,7 @@ public class ExtraBallUpgrade : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D _)
     {
-        var newBall = Instantiate(NewBallPrefab);
+        var newBall = Instantiate(NewBallPrefab, FindObjectOfType<Paddle>().transform.position + Vector3.up, Quaternion.identity);
         newBall.GetComponent<Ball>().Launch();
     }
 }
